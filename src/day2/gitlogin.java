e393054208ff6a6fca1f202f7201f41b9cc918a2
rugved18 <rugvedkulk2003@gmail.com>
2024-07-02 07:12:27 +0530

    updated

---
+++ src/day2/gitlogin.java
@@ -0,0 +1,10 @@
+package day2;
+
+public class gitlogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("git login is done");
+	}
+
+}
